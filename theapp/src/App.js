bee3c8990d3cb4f7b1428784f7ab73265360f0f9
Duraan Ali <duraan522@gmail.com>
2019-09-06 13:28:16 -0500

    added react app DONE WITH STRETCH

---
+++ theapp/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Projects from './components/Projects';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        <Projects />
+      </header>
+    </div>
+  );
+}
+
+export default App;
